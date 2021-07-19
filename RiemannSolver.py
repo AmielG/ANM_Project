bc02aca450d4d2d4a4add082ab84eca8abfaff78
Amiel Giloni <amiel@marmelada.co.il>
2021-07-19 16:45:33 +0300

    Update RiemannSolver.py

--- RiemannSolver.py
+++ RiemannSolver.py
@@ -20,7 +20,7 @@ class RiemannSolver:
         self.a_l = self.calc_speed_of_sound(self.temp_l)
         self.temp_r = self.calc_ideal_gas_temp(self.p_r, self.rho_r)
         self.a_r = self.calc_speed_of_sound(self.temp_r)
-        self.p_1 = self.calc_p_1(self.p_r*2)
+        self.p_1 = self.calc_p_1(self.p_r * 2)
 
     def calc_speed_of_sound(self, temp):
         return np.sqrt(self.gamma * self.R * temp)
@@ -32,9 +32,23 @@ class RiemannSolver:
         delta = 1
         p_1 = p_1_guss
         while delta > 0.001:
-            p_1_new = self.p_l * (1 - (self.gamma-1)*(self.a_r/self.a_l)*((p_1 / self.p_r)-1) / (4*self.gamma**2+2*self.gamma*(self.gamma + 1)*((p_1/self.p_r)-1)))
+            p_1_new = self.p_l * (1 - (self.gamma - 1) * (self.a_r / self.a_l) * ((p_1 / self.p_r) - 1) / (
+                        4 * self.gamma ** 2 + 2 * self.gamma * (self.gamma + 1) * ((p_1 / self.p_r) - 1)))
             delta = abs(p_1_new - p_1)
             p_1 = p_1_new
         return p_1
 
+    def calc_temp_1(self):
+        a = self.p_1 / self.p_r
+        b = (self.gamma + 1) / (self.gamma - 1)
+        return self.temp_r * a * ((a + b) / (1 + a * b))
 
+    def calc_rho_1(self):
+        a = self.p_1 / self.p_r
+        b = (self.gamma + 1) / (self.gamma - 1)
+        return self.rho_r * ((1 + a * b) / (a + b))
+
+    def calc_velocity_1(self):
+        a = self.p_1 / self.p_r
+        b = (self.gamma - 1) / (self.gamma + 1)
+        return (self.a_r / self.gamma) * (a -1) * ((2*self.gamma/(self.gamma+1))/(b + a)) ** 0.5
